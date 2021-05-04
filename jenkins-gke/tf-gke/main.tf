a4c6b1e949660dcc679a8e3355d57eecb60c984f
gauravkr19 <gauravkr19@gmail.com>
2021-05-04 07:39:15 +0000

    SA as create

--- jenkins-gke/tf-gke/main.tf
+++ jenkins-gke/tf-gke/main.tf
@@ -1,19 +1,13 @@
-/**
- * Copyright 2020 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+/*
+iam.googleapis.com
+cloudresourcemanager.googleapis.com
+compute.googleapis.com
+containerregistry.googleapis.com
+container.googleapis.com
+storage-component.googleapis.com
+logging.googleapis.com
+monitoring.googleapis.com
+*/
 
 /*****************************************
   Activate Services in Jenkins Project
@@ -86,7 +80,7 @@ module "jenkins-gke" {
   logging_service          = "logging.googleapis.com/kubernetes"
   monitoring_service       = "monitoring.googleapis.com/kubernetes"
   remove_default_node_pool = true
-  service_account          = "terraform-sa@playground-s-11-4d96fe3f.iam.gserviceaccount.com"
+  service_account          = "create"
   identity_namespace       = "${module.enables-google-apis.project_id}.svc.id.goog"
   node_metadata            = "GKE_METADATA_SERVER"
   node_pools = [
